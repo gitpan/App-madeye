09e43739f69d0ef87e72f55f5aae5e0bd9d7d96e
MATSUNO★Tokuhiro <tokuhirom+cpan@gmail.com>
2008-03-21 08:42:03 +0000

    Import of TOKUHIROM/App-MadEye-0.04 from CPAN.
    gitpan-cpan-distribution: App-madeye
    gitpan-cpan-version:      0.04
    gitpan-cpan-path:         TOKUHIROM/App-MadEye-0.04.tar.gz
    gitpan-cpan-author:       TOKUHIROM
    gitpan-cpan-maturity:     released

--- lib/App/MadEye.pm
+++ lib/App/MadEye.pm
@@ -2,7 +2,7 @@ package App::MadEye;
 use strict;
 use warnings;
 use 5.00800;
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 use Class::Component;
 use Params::Validate;
 use UNIVERSAL::require;
@@ -123,7 +123,7 @@ sub log {
     die "missing level" unless $level;
     die "missing msg" unless $msg;
 
-    $self->{logger}->log( level => $level, message => "[$level]$msg\n" );
+    $self->{logger}->log( level => $level, message => "[$level] $msg\n" );
 }
 
 1;
--- lib/App/MadEye/Util.pm
+++ lib/App/MadEye/Util.pm
@@ -3,32 +3,40 @@ use strict;
 use warnings;
 use base qw/Exporter/;
 
-our @EXPORT = qw/timeout log_stopwatch get_schema_from_pod/;
+our @EXPORT = qw/timeout get_schema_from_pod context snmp_session/;
 
 use Sys::Syslog qw/:DEFAULT/;
 use Pod::POM ();
 use List::Util qw/first/;
 use YAML ();
 use Time::HiRes qw/gettimeofday/;
+use Net::SNMP;
+
+sub context () { App::MadEye->context } ## no critic.
 
 sub timeout($$&) {    ## no critic.
     my ( $secs, $msg, $code ) = @_;
-    App::MadEye->context->log(debug => "run timer: '$msg', $secs");;
+
+    context->log(debug => "run timer: '$msg', $secs");;
+
     my $last_alarm = 0;
+    my $err;
     eval {
         local $SIG{ALRM} = sub { die "Time out error: $msg" };
         $last_alarm = alarm $secs;
 
         my $start_time = gettimeofday();
             $code->();
-        App::MadEye->context->log('debug' => "stopwatch: " . (gettimeofday() - $start_time));
+        context->log('debug' => "stopwatch: " . (gettimeofday() - $start_time));
     };
     if ($@) {
-        my $err = $@;
-        App::MadEye->context->log('error' => $err);
-        warn $err;
+        $err = $@;
+
+        context->log('error' => $err);
     }
     alarm $last_alarm; # restore
+
+    return $err;
 }
 
 sub get_schema_from_pod {
@@ -46,5 +54,29 @@ sub get_schema_from_pod {
         return; # 404 schema not found.
     }
 }
+
+sub snmp_session {
+    my ($agent, $host, $callback, ) = @_;
+
+    my $community = $agent->config->{config}->{community} or die "missing community";
+    my $port      = $agent->config->{config}->{port}    || 161;
+    my $timeout   = $agent->config->{config}->{timeout} || 10;
+
+    my ($session, $error) = Net::SNMP->session(
+        -hostname  => $host,
+        -community => $community,
+        -port      => $port,
+        -timeout   => $timeout,
+    );
+
+    if (not defined($session)) {
+        die "ERROR: $error.\n";
+    } else {
+        my $response = $callback->($session);
+        $session->close();
+        return $response;
+    }
+}
+
 
 1;
